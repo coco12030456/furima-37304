aa99ea1e03d963592d6b8dd714811cf760155ffa
coco12030456 <nkm.1.2525@ezweb.ne.jp>
2021-12-22 16:59:07 +0900

    バリデーションの追加,ログインページの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   
-   validates :name, presence: true
-  
+  with_options presence: true do
+    validates :name
+    validates :date_of_birth_id
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 end
