a08b67c63fbc576536b52e68dd824c7926071c5f
coco12030456 <nkm.1.2525@ezweb.ne.jp>
2021-12-23 11:46:48 +0900

    Active Storageの導入,ストロングパラメータの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+  private
+
+  def prototype_params
+    params.require(:item).permit(:title, :info, :price, :category_id, :condition_id, :send_price_id, :area_id, :send_day_id, :image).merge(user_id: current_user.id)
+  end
 end
