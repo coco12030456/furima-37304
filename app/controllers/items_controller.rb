4c6b79ca18f93e128184c877f279d94c591bb5ce
coco12030456 <nkm.1.2525@ezweb.ne.jp>
2021-12-24 20:23:34 +0900

    役割が重複している記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
